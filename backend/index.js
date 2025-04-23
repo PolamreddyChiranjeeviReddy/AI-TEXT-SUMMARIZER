6e52d31c82405359f6813f43abfdbad3e045d7ba
PolamreddyChiranjeeviReddy <cjr45555@gmail.com>
2025-04-23 23:00:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const summarizeText = require('./summarize');
 const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;  // Use the port from environment or default to 3000
+;
 
 app.use(express.json());  // Built-in middleware to parse JSON requests
 app.use(cors())
